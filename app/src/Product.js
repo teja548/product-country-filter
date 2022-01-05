e5e680f8f7fc686acbe26a2d582f4b4735d79800
teja548 <tet9692@gmail.com>
2022-01-05 19:34:38 +0530

    currency

--- app/src/Product.js
+++ app/src/Product.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Product.css';
 
+
 const Product = ({ product }) => {
+
     return (
         <div className='productCard__wrapper'>
             <div>
                 <img className='productCard__img' src={product.image} alt='' />
                 <h4>{product.name}</h4>
                 <div className='ProductCard__price'>
-                    <h5>{product.price}</h5>
+                    <h5>${product.price}</h5>
                     <h5>{product.desc}</h5>
                 </div>
             </div>
